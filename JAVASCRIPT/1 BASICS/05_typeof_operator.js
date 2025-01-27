d6b5db1206fcd9b58526ccdd15e3b0dd623ec63a
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-01-27 17:36:31 +0530

    Added 05 typeof operator

---
+++ JAVASCRIPT/1 BASICS/05_typeof_operator.js
@@ -0,0 +1,12 @@
+// typeof function is used to identify the datatype of a variable or value
+
+// Syntax: typeof variable/value
+
+console.log(typeof 5); // number
+console.log(typeof 2.7); // number (float)
+console.log(typeof "Hare Krsna"); // string
+console.log(typeof true); // boolean
+console.log(typeof(5 !== '5')); // boolean
+
+
+
