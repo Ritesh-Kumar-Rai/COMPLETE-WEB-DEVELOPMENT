ee192202a7459ff9cafcf55436932bf1379fd6b7
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-02-03 00:38:04 +0530

    Removed the mistake which i have done previously in if else statement where i use isIndianCitizen==='false' to false; and write more optimized approach. + solved some interview questions related to control statement and covered ternary operator.

--- JAVASCRIPT/1 BASICS/14_control_statements.js
+++ JAVASCRIPT/1 BASICS/14_control_statements.js
@@ -20,18 +20,67 @@ if(kanpur_temperature >= 20){
 }
 
 // Practice
-const person_age = 15;
+
+//! Requirements:
+//? If the person is 18 years or older, a citizen, and registered to vote, display a message saying they are eligible to vote.
+//? If the person is younger than 18, not a citizen, or not registered to vote, display a message saying they are not eligible to vote.
+//? If the person is 18 or older but not a citizen, display a message saying they are not eligible due to citizenship status.
+//? If the person is 18 or older, a citizen, but not registered to vote, display a message saying they are not eligible due to registration status.
+//? Extended voting eligibility checker with additional conditions
+
+const person_age = 18;
 const isRegisteredForVote = true;
 const isIndianCitizen = false;
 
-if(person_age >= 18 && isRegisteredForVote && isIndianCitizen){
+/*if(person_age >= 18 && isRegisteredForVote && isIndianCitizen){
   console.log("You are eligible for vote.");
-}else if(person_age < 18 && isRegisteredForVote === 'false' && isIndianCitizen === 'false'){
+}else if(person_age < 18 && isRegisteredForVote === false && isIndianCitizen === false){
   console.log('You are not eligible for vote!');
-}else if(person_age >= 18 && isIndianCitizen === 'false'){
+}else if(person_age >= 18 && isIndianCitizen === false){
   console.log('You are not eligible for vote due to citizenship status!');
-}else if(person_age >= 18 && isIndianCitizen && isRegisteredForVote === "false"){
+}else if(person_age >= 18 && isIndianCitizen && isRegisteredForVote === false){
   console.log("You are not eligible for vote due to registration status!");
 }else{
   console.log("Oops! You are not eligible for vote!");
+}*/
+
+// Optimized for readability
+if(person_age >=18){
+  if(isIndianCitizen){
+      if(isRegisteredForVote){
+        console.log("You are eligible for vote.");
+      }else{
+        console.log("You are not eligible for vote due to registration status!");
+      }
+  }else{
+    console.log('You are not eligible for vote due to citizenship status!');
+  }
+}else{
+  console.log("You are not eligible for vote! because you are younger");
 }
+
+// Interview questions:
+
+// 1: Write a program to check if a number is even or odd.
+const num = "6";
+
+if(num % 2 === 0){
+  console.log("Number is Even.");
+}else{
+  console.log("Number is Odd.");
+}
+
+// 2: Write a program to check if a number is positive, negative, or zero.
+const check = -1;
+if(check === 0){
+  console.log("Number is Zero");
+}else if(check > 0){
+  console.log("Number is Positive");
+}else{
+  console.log("Number is Negative");
+}
+
+// Ternary Operator: condition ? true : false;
+const userLoggedIn = true;
+userLoggedIn ? console.log("User LoggedIn") : console.log("User not LoggedIn");
+ 
