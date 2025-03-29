e164229e8f05374eb03567bed2e9652b526a8744
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-03-29 17:28:47 +0530

    In JAVASCRIPT Folder/Projects/E-Commerce Web Application, created an app.js where i created a function for Navbar cartbtn where i added a eventListener and done a navigation to cart.html

---
+++ JAVASCRIPT/Projects/E-Commerce Web Application/scripts/app.js
@@ -0,0 +1,42 @@
+// JS code for page navigation when user clicks on navbar cart btn
+
+const navigateToCart = (page_type) =>{
+  if(!page_type){
+    throw new ReferenceError("We didn't get attribute from navbar add-to-cart-btn");
+  }
+
+  const baseUrl = window.location.origin + "/JAVASCRIPT/Projects/E-Commerce Web Application/";
+
+    switch(page_type){
+      case "index.html":
+        window.location.href = baseUrl +"./pages/cart.html";
+        break;
+
+      case "allProducts.html":
+        window.location.href = baseUrl + "./pages/cart.html";
+        break;
+
+      case "product.html":
+        window.location.href = baseUrl + "./pages/cart.html";
+        break;
+
+      default:
+        throw new TypeError("We failed to get current page name after clicked on Cart Btn. (error from navbar cart button)");
+              
+    }
+}
+
+document.addEventListener("DOMContentLoaded",()=>{
+  const navAdd_to_CartBtn = document.getElementById("navbar-add-to-cart-btn");
+
+  navAdd_to_CartBtn.addEventListener('click',()=>{
+    const page_type = navAdd_to_CartBtn.getAttribute("data-page-type");
+    console.log(page_type);
+    try {
+      navigateToCart(page_type);
+    } catch (error) {
+      console.error(`${error.name} -> ${error.message}`);
+    }
+    
+  })
+});
