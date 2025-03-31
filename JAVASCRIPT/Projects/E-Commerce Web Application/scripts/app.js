6cd523715cc7fc7a47a830399975e484ede31728
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-03-31 23:14:13 +0530

    In JAVASCRIPT Folder/Projects/E-Commerce Web Application, in app.js added a new code for Navbar wishlist button to listen it and when user clicks on it navigate to wishlist.html.

--- JAVASCRIPT/Projects/E-Commerce Web Application/scripts/app.js
+++ JAVASCRIPT/Projects/E-Commerce Web Application/scripts/app.js
@@ -1,46 +1,98 @@
 // JS code for page navigation when user clicks on navbar cart btn
 
-const navigateToCart = (page_type) =>{
-  if(!page_type){
-    throw new ReferenceError("We didn't get attribute from navbar add-to-cart-btn");
+const navigateToCart = (page_type) => {
+  if (!page_type) {
+    throw new ReferenceError("We didn't get attribute from navbar go-to-cart-btn");
   }
 
   const baseUrl = window.location.origin + "/JAVASCRIPT/Projects/E-Commerce Web Application/";
 
-    switch(page_type){
-      case "index.html":
-        window.location.href = baseUrl +"./pages/cart.html";
-        break;
+  switch (page_type) {
+    case "index.html":
+      window.location.href = baseUrl + "./pages/cart.html";
+      break;
 
-      case "allProducts.html":
-        window.location.href = baseUrl + "./pages/cart.html";
-        break;
+    case "allProducts.html":
+      window.location.href = baseUrl + "./pages/cart.html";
+      break;
 
-      case "product.html":
-        window.location.href = baseUrl + "./pages/cart.html";
-        break;
+    case "product.html":
+      window.location.href = baseUrl + "./pages/cart.html";
+      break;
 
-      case "wishlist.html":
-      window.location.href = baseUrl + "./pages/cart.html";  
-      break;  
+    case "wishlist.html":
+      window.location.href = baseUrl + "./pages/cart.html";
+      break;
 
-      default:
-        throw new TypeError("We failed to get current page name after clicked on Cart Btn. (error from navbar cart button)");
-              
-    }
+    default:
+      throw new TypeError("We failed to get current page name after clicked on Cart Btn. (error from navbar cart button)");
+
+  }
 }
 
-document.addEventListener("DOMContentLoaded",()=>{
-  const navAdd_to_CartBtn = document.getElementById("navbar-add-to-cart-btn");
+// JS code for page navigation when user clicks on wishlist btn
 
-  navAdd_to_CartBtn.addEventListener('click',()=>{
-    const page_type = navAdd_to_CartBtn.getAttribute("data-page-type");
-    console.log(page_type);
-    try {
-      navigateToCart(page_type);
-    } catch (error) {
-      console.error(`${error.name} -> ${error.message}`);
-    }
-    
-  })
+const navigateToWishlist = (page_type) => {
+  if (!page_type) {
+    throw new ReferenceError("We didn't get attribute from navbar wishlist-btn");
+  }
+
+  const baseUrl = window.location.origin + "/JAVASCRIPT/Projects/E-Commerce Web Application/";
+
+  switch (page_type) {
+    case "index.html":
+      window.location.href = baseUrl + "./pages/wishlist.html";
+      break;
+
+    case "allProducts.html":
+      window.location.href = baseUrl + "./pages/wishlist.html";
+      break;
+
+    case "product.html":
+      window.location.href = baseUrl + "./pages/wishlist.html";
+      break;
+
+    case "cart.html":
+      window.location.href = baseUrl + "./pages/wishlist.html";
+      break;
+
+    default:
+      throw new TypeError("We failed to get current page name after clicked on Wishlist Btn. (error from navbar cart button)");
+
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  try {
+    // code for cart btn
+    const navCartBtn = document.getElementById("navbar-go-to-cart-btn");
+
+    navCartBtn.addEventListener('click', () => {
+      const page_type = navCartBtn.getAttribute("data-page-type");
+      console.log(page_type);
+      try {
+        navigateToCart(page_type);
+      } catch (error) {
+        console.error(`${error.name} -> ${error.message}`);
+      }
+
+    });
+
+  } catch (error) {
+    console.error(`${error.name} -> ${error.message}`);
+  }
+
+  try {
+    // code for wishlist btn
+    const navWishlistBtn = document.getElementById("navbar-wishlist-btn");
+
+    navWishlistBtn.addEventListener('click', () => {
+      const page_type = navWishlistBtn.getAttribute("data-page-type");
+      navigateToWishlist(page_type);
+    });
+  } catch (error) {
+    console.error(`${error.name} -> ${error.message}`);
+  }
+
 });
