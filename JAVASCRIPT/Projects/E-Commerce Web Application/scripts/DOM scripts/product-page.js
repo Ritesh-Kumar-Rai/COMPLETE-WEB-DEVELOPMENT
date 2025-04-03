c0a19a6b0f3f180ae23964c96e54ff2fdc2d476b
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-04-03 19:06:35 +0530

    In JAVASCRIPT Folder/Projects/E-Commerce Web Application, created a product-page.js which is reponsible to get a id from url bar and display the product info.

---
+++ JAVASCRIPT/Projects/E-Commerce Web Application/scripts/DOM scripts/product-page.js
@@ -0,0 +1,105 @@
+// import class
+import RenderCards from "../DOM scripts/RenderCards.js";
+
+// Code for product.html
+
+// Get the current URL
+const url = window.location.href;
+
+// Creating a URL object
+const urlObj = new URL(url);
+
+// Creating a URLSearchParams object from the URL's query string
+const params = urlObj.searchParams;
+
+// Getting the 'id' parameter value
+const productId = parseInt(params.get('id'));
+
+// Log or use the productId as needed
+console.log(productId); // This will print the id
+
+const fetchSingleProductData = (id) =>{
+  if(!id || typeof(id) !== "number"){
+    // console.warn(id, typeof id)
+    throw new ReferenceError("id as parameter is required.");
+  }
+
+  fetch(`https://fakestoreapi.in/api/products/${id}`)
+  .then(res => res.json())
+  .then((response) =>{
+    console.log(response.status);
+    const category_type = printProductInfo(response.product);
+    fetchandRenderProduct_byCategory(category_type);
+  })
+  .catch(err => console.error(`${err.name} -> ${err.message}`));
+}
+
+const breadcrumProductName = document.getElementById("breadcrum-product-name");
+const productTitle = document.getElementById("product-title");
+const productImage = document.getElementById("product-image");
+const productBrand = document.getElementById("product-brand");
+const productModel = document.getElementById("product-modelname");
+const productCategoryType = document.getElementById("product-category-type");
+const productColorType = document.getElementById("product-color-type");
+const productDiscountedPriceElem = document.querySelector(".discounted-price");
+const productActualPriceElem = document.querySelector(".actual-price");
+const productDiscountPercentElem = document.querySelector(".discount-percent");
+const productDescriptionElem = document.getElementById("product-description");
+
+// function to calculate dicounted price
+const calculateDiscountedPrice = (amount, discount) =>{
+  const discountAmount = (discount/100) * amount;
+  const discountedPrice = amount - discountAmount;
+  return discountedPrice;
+}
+
+const printProductInfo = (productObj) =>{
+  if(!productObj){
+    throw new ReferenceError("productObj as parameter is Required!");
+  }else if(typeof(productObj) !== "object" || Object.prototype.toString.call(productObj) !== "[object Object]"){
+    throw new TypeError("Parameter productObj is not an object type!");
+  }else if(Object.keys(productObj).length < 4){
+    throw new RangeError("The provided product info. object is not complete object!");
+  }
+
+  breadcrumProductName.textContent = productObj?.title || "Oops! We failed to get title";
+  productTitle.textContent = productObj?.title || "Oops! We failed to get title";
+  productImage.src = productObj.image;
+  productBrand.textContent = productObj.brand;
+  productModel.textContent = productObj.model;
+  productCategoryType.textContent = productObj.category;
+  productColorType.textContent = productObj.color;
+  productDiscountedPriceElem.textContent = "₹" + calculateDiscountedPrice(Number(productObj.price), Number(productObj.discount))
+  productActualPriceElem.textContent = "₹"+ productObj.price;
+  productDiscountPercentElem.textContent = `(-${productObj.discount}% off)`;
+  productDescriptionElem.innerHTML = productObj.description.replace(/(\r\n|\n|\r)/g, '<br><br>');
+
+  return productObj.category;
+}
+
+const fetchandRenderProduct_byCategory = async (category_name) =>{
+  try {
+    const response = await fetch(`https://fakestoreapi.in/api/products/category?type=${category_name}`);
+
+    const refinedResponse = await response.json();
+    const productsArray =  await (refinedResponse.products).slice(0,5);
+    const similarProElem = document.querySelector(".similar-products-body");
+    
+    setTimeout(()=>{
+      RenderCards.renderProductCards(similarProElem, productsArray);
+    },10000);
+
+  } catch (error) {
+    console.error(`${error.name} -> ${error.message}`);
+  }
+}
+
+try {
+  const similarProductContainer = document.querySelector(".similar-products-body");
+  console.log(similarProductContainer)
+  RenderCards.renderSkeletonCards(similarProductContainer, 5);
+  fetchSingleProductData(productId);
+
+} catch (error) {
+  console.error(`${error.name} -> ${error.message}`);
+}
