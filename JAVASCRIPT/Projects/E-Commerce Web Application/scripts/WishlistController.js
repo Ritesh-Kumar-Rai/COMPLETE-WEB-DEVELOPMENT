5eaa5f560b1d990653b798df4e3480c0978f30c3
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-04-14 22:42:04 +0530

    In JAVASCRIPT Folder/Projects/E-Commerce Web Application, added a methods to WishlistController.js class for cookies to save wishlist array data to cookie.

--- JAVASCRIPT/Projects/E-Commerce Web Application/scripts/WishlistController.js
+++ JAVASCRIPT/Projects/E-Commerce Web Application/scripts/WishlistController.js
@@ -1,65 +1,121 @@
 // classes for wishlist related methods and values
 
 // custom error class
-class WishlistError extends Error{
-  constructor(message){
+class WishlistError extends Error {
+  constructor(message) {
     super(message);
     this.name = "WishlistError";
   }
 }
 
 // wishlist main class
-class WishlistController{
+class WishlistController {
   #WISHLISTED_PRODUCTS = [];
-  constructor(){
+  constructor() {
+    const cookieName = "logoIpsum-Wishlist-Cookie";
+    this.#WISHLISTED_PRODUCTS = this.#getCookie(cookieName) || [];
     console.warn("constructor of WishlistController is executed...");
   }
 
-  getWishlistData(){
+  getWishlistData() {
     const stored_wishlist_data = this.#WISHLISTED_PRODUCTS;
     return stored_wishlist_data;
   }
 
-  push_product_to_wishlist(id){
-    if(!id){
+  push_product_to_wishlist(id) {
+    if (!id) {
       throw new WishlistError("'id' parameter is missing!");
-    }else if(typeof(id) !== 'number'){
+    } else if (typeof (id) !== 'number') {
       throw new WishlistError("'id' parameter is expected as Numeric type!");;
     }
-    if(this.isAvailableInWishlist(id)){
+    if (this.isAvailableInWishlist(id)) {
       this.remove_product_from_wishlist(id);
       return false;
     }
     this.#WISHLISTED_PRODUCTS.push(id);
     console.log('product added to wishlist');
-    console.log(this.#WISHLISTED_PRODUCTS)
+    console.log(this.#WISHLISTED_PRODUCTS);
+    this.#saveWishlistData(); // invoking this method to save the wishlist data to cookie/session [after adding data to wishlist array...]
     return true;
   }
 
-  remove_product_from_wishlist(id){
-    if(!id){
+  remove_product_from_wishlist(id) {
+    if (!id) {
       throw new WishlistError("'id' parameter is missing!");
-    }else if(typeof(id) !== 'number'){
+    } else if (typeof (id) !== 'number') {
       throw new WishlistError("'id' parameter is expected as Numeric type!");;
     }
 
     const index_to_remove = this.#WISHLISTED_PRODUCTS.indexOf(id);
     this.#WISHLISTED_PRODUCTS.splice(index_to_remove, 1);
-    if(this.isAvailableInWishlist(id)){
+    if (this.isAvailableInWishlist(id)) {
       throw new WishlistError("We tried to remove product from wishlist but still it exists! How? try to find the reason. Strange");
     }
     console.warn("product is successfully removed from wishlist!");
+    this.#saveWishlistData(); // invoking this method to save the wishlist data to cookie/session [after adding data to wishlist array...]
   }
 
-  isAvailableInWishlist(id){
-    if(!id){
+  isAvailableInWishlist(id) {
+    if (!id) {
       throw new WishlistError("'id' parameter is missing!");
-    }else if(typeof(id) !== 'number'){
+    } else if (typeof (id) !== 'number') {
       throw new WishlistError("'id' parameter is expected as Numeric type!");;
     }
 
     return this.#WISHLISTED_PRODUCTS.includes(id);
   }
+
+  #saveWishlistData() {
+    // method to store/save wishlist data to Cookie/Session
+    const cookieName = "logoIpsum-Wishlist-Cookie";
+    this.#setCookie(cookieName, this.#WISHLISTED_PRODUCTS, 1, "minutes");
+    alert(this.#getCookie(cookieName));
+  }
+
+  // method to save cookies with options for minutes, hours, or days
+  #setCookie(name, value, duration, unit) {
+    const date = new Date();
+
+    // Calculate expiration time based on the unit (minutes, hours, days)
+    switch (unit) {
+      case "minutes":
+        date.setTime(date.getTime() + (duration * 60 * 1000)); // Convert minutes to milliseconds
+        break;
+      case "hours":
+        date.setTime(date.getTime() + (duration * 60 * 60 * 1000)); // Convert hours to milliseconds
+        break;
+      case "days":
+        date.setTime(date.getTime() + (duration * 24 * 60 * 60 * 1000)); // Convert days to milliseconds
+        break;
+      default:
+        console.error("Invalid time unit! Use 'minutes', 'hours', or 'days'.");
+        return;
+    }
+
+    // Create cookie with expiration date
+    const expires = `expires=${date.toUTCString()}`;
+    document.cookie = `${encodeURIComponent(name)}=${encodeURIComponent(JSON.stringify(value))}; ${expires}; path=/`;
+    console.log(`Cookie "${name}" set with expiration: ${date}`);
+  }
+
+  // method to get data from a cookie
+  #getCookie(name) {
+    const cookies = document.cookie.split("; ");
+    for (let cookie of cookies) {
+      const [key, value] = cookie.split("=");
+      if (decodeURIComponent(key) === name) {
+        return JSON.parse(decodeURIComponent(value)); // Parse the JSON back into an array/object
+      }
+    }
+    return null; // Return null if the cookie doesn't exist
+  }
+
+  // method to delete a cookie
+  #deleteCookie(name) {
+    document.cookie = `${encodeURIComponent(name)}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/`;
+    console.log(`Cookie "${name}" has been deleted.`);
+  }
+
 }
 
 export default WishlistController;
