bcb8a60a60173015fb02a19b085a885c45a708d1
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-04-25 00:07:13 +0530

    In JAVASCRIPT Folder/Projects/E-Commerce Web Application, Just experimented in CartController.js by writing some logic on add_product_to_CART

--- JAVASCRIPT/Projects/E-Commerce Web Application/scripts/CartController.js
+++ JAVASCRIPT/Projects/E-Commerce Web Application/scripts/CartController.js
@@ -20,12 +20,28 @@ class CartController{
     return temp_cart_arr;
   }
 
-  add_product_to_CART(id){
+  add_product_to_CART(id, input_value){
+    // const qtyData = JSON.parse(sessionStorage.getItem("logoIpsum-product-quantity-data")) || [{product_id: 5, qty: 45}];
+    const qtyData = [{product_id: 5, qty: 44}]
+    const productQty = Number(qtyData.find( each_obj => each_obj.product_id === id)?.qty) || null;
 
+    if(!productQty){
+      throw new CartError("We are unable to get the product Quantity!" + productQty);
+    }
+    console.log(`product id is ${id} which has quantity : ${productQty}`);
+    if(!this.#CART.includes(id)){
+      this.#CART.push(id);
+    }
+    qtyData.forEach((each_obj)=>{
+      if(each_obj.product_id === id){
+        each_obj.qty++;
+      }
+    })
+    console.log(qtyData);
   }
 
   remove_product_from_CART(id){
-    
+
   }
 
   isAvailableInCART(id){
@@ -33,3 +49,14 @@ class CartController{
   }
 }
 
+const obj = new CartController();
+
+try {
+  obj.add_product_to_CART(5,89);
+  const arr = obj.getCARTData();
+  console.log(arr);
+  obj.add_product_to_CART(2,89);
+  console.log(obj.getCARTData());
+} catch (error) {
+  console.error(error.name, error.message);
+}
