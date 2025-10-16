cc86524d24f56ffc39fe64d531ce948be6df91ff
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-10-16 23:58:16 +0530

    I started MySQL & Created a another repo inside web_dev root repo for MySQL, where i was pushing first code for creation and deletion of database; I already completed React js from different repo which is from 2023-2024 React repo;

---
+++ MySQL/01_create_database.sql
@@ -0,0 +1,15 @@
+-- How to create a database?
+
+CREATE DATABASE learning_mysql;
+
+-- How to delete a database?
+
+DROP DATABASE learning_mysql;
+
+-- How to check if the database exists before creating it
+
+CREATE DATABASE IF NOT EXISTS learning_mysql2;
+
+-- How to delete the database if it exists
+
+DROP DATABASE IF EXISTS learning_mysql2;
