5f5ba9b60509f44e20cfe82bf71f02477a120f70
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-11-06 16:13:40 +0530

    In MySQL added 02 Create Table in MySQL

---
+++ MySQL/02_create_table.sql
@@ -0,0 +1,39 @@
+# Creating Table
+
+-- USE databaseName; to select the db where we will create table
+USE startersql;
+
+CREATE TABLE users(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('Male', 'Female', 'Others'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Data Types Explained
+/*
+
+* INT : Integer type, used for whole numbers.
+* VARCHAR(100) : Variable-length string, up to 100 characters.
+* ENUM : A string object with a value chosen from a list of permitted values. eg.
+        gender ENUM('Male', 'Female', 'Other')
+* DATE : Stores date values. eg date_of_birth DATE
+* TIMESTAMP : Stores date and time, automatically set to the current timestamp
+            when a row is created.
+* BOOLEAN : Stores TRUE or FALSE values, often used for flags like is_active .
+* DECIMAL(10, 2) : Stores exact numeric data
+values, useful for financial data. The first number is the total number of digits, and the second is the number of digits after the decimal point.
+*/
+
+-- Constraints Explained
+/*
+- AUTO_INCREMENT : Automatically generates a unique number for each row.
+- PRIMARY KEY : Uniquely identifies each row in the table.
+- NOT NULL : Ensures a column cannot have a NULL value.
+- UNIQUE : Ensures all values in a column are different.
+- DEFAULT : Sets a default value for a column if no value is provided. eg.
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP , is_active BOOLEAN
+            DEFAULT TRUE
+*/
