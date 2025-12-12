28e7b62bc8492fe4c39dd85de7968d3d6ed551cc
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-12-12 23:49:45 +0530

    In nodejs folder, added 01_data_modeling where i learn how to design,make and work with data models of different projects

---
+++ NODEJS/01_Data_Modeling_in_MongoDB/models/todos/todos.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    color: {
+      type: String,
+    },
+    subTodos: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SubTodo",
+      },
+    ],
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Todo = mongoose.model("Todo", todoSchema);
