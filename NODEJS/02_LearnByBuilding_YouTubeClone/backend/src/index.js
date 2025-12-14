5004e9829672d57e2c5fa365741c36ef6cbba7e9
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-12-15 01:59:02 +0530

    Refactored and modularized the express code migrating from index.js to app.js and exported `app` instance of express. Plus installed cookie-parser & cors and configured basic but important settings in app.js by using app.use()

--- NODEJS/02_LearnByBuilding_YouTubeClone/backend/src/index.js
+++ NODEJS/02_LearnByBuilding_YouTubeClone/backend/src/index.js
@@ -1,17 +1,22 @@
 import dotenv from "dotenv";
 import connect_to_mongodb from "./db/index.js";
-import express from "express";
+import app from "./app.js";
 
 dotenv.config({ path: "./.env" });
 
-const app = express();
-
 console.log("connecting to mongodb...");
 
-connect_to_mongodb().then((e) => {
-  app.listen(process.env.PORT, () => {
-    console.log(
-      `Server is started serving at http://localhost:${process.env.PORT}`
-    );
+connect_to_mongodb()
+  .then((e) => {
+    app.on("error", (err) => {
+      console.log("Something went wrong!");
+      throw err;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is started serving at http://localhost:${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Failed while connecting to mongodb and starting a server!\n", error);
   });
-});
