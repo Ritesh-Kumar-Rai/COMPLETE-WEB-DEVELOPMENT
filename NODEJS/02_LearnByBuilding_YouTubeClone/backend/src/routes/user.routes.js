c2c85460eb53f06b04913dc1af2860f02cb6b442
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2025-12-18 00:05:44 +0530

    used those User Router, User.model, multer file for register user controller and completed the user register controller function. All fields are accepting, image was successfully uploaded to ./public/upload but extension was missing! and while storing data to mongodb problem occurred will see tomorrow.

--- NODEJS/02_LearnByBuilding_YouTubeClone/backend/src/routes/user.routes.js
+++ NODEJS/02_LearnByBuilding_YouTubeClone/backend/src/routes/user.routes.js
@@ -2,12 +2,16 @@
 
 import { Router } from "express";
 import { registerUser } from "../controllers/users.controller.js";
+import upload from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
 // register user route
 
-router.route("/register").post(registerUser);
+router.route("/register").post(upload.fields([
+    {name: "avatar", maxCount: 1},
+    {name: "coverImage", maxCount: 1}
+]),registerUser);
 
 
 export default router;
