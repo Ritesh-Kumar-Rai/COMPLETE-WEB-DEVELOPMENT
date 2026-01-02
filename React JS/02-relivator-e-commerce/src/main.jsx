7b40261364d21b45900502864e474d32f47202b9
Ritesh Kumar Rai <the.ritesh.kumar.rai@gmail.com>
2026-01-02 17:02:44 +0530

    By Mistakenly named the BrowseProducts component to ProductDetails now re-named.

--- React JS/02-relivator-e-commerce/src/main.jsx
+++ React JS/02-relivator-e-commerce/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home'
-import ProductDetails from './pages/ProductDetails'
+import BrowseProducts from './pages/BrowseProducts'
 
 const router = createBrowserRouter([
   {
@@ -16,7 +16,7 @@ const router = createBrowserRouter([
     },
     {
       path: '/browse-products',
-      element: <ProductDetails />
+      element: <BrowseProducts />
     }]
   }
 ]);
